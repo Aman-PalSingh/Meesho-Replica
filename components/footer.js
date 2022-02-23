56b531062f8c82341c061fcc5785e47ff51b8954
Aman-PalSingh <amanpalsingh777@gmail.com>
2022-02-23 21:38:42 +0530

    footer added to home page
    footer imported from footer.js in components folder

---
+++ components/footer.js
@@ -0,0 +1,157 @@
+ export {footer,aboutMeesho} ;
+function footer() {
+    return `<div>
+           <h2>Shop No-Stop on Meesho</h2>
+           <p>Trusted by more than 1 Crore Indians
+            Cash on Delivery | Free Delivery</p>
+            <img src="https://meesho.com/_next/static/images/google-play-button-1f2aa747a3eefa161840b04e057b31ac.png"; alt="" width="47%">
+            <img src="https://meesho.com/_next/static/images/appstore-button-4b171cf04fe0557718dfd2cbf309c61d.png" alt="" width="47%
+            ">
+       </div>
+       <div>
+           <a href="">Careers</a>
+           <a href="">Become a supplier</a>
+       </div>
+       <div>
+           <a href="">Legal And Policies</a>
+           <a href="">Meesho Tech Blog</a>
+       </div>
+       <div>
+           <a href=""><h3>Contact Us</h3></a>
+           <a href="">query@meesho.com</a>
+       </div>
+       <div>
+           <a href="">Reach out to us</a>
+           <img src="https://meesho.com/assets/facebook.png" alt="">
+           <img src="https://meesho.com/assets/instagram.png" alt="">
+           <img src="https://meesho.com/assets/youtube.png" alt="">
+           <img src="https://meesho.com/assets/linkedin.png" alt="">
+           <img src="https://meesho.com/assets/twitter.png" alt="">
+
+       </div>
+
+   </div> `
+}
+    function aboutMeesho()
+    {
+             return `<button id="expansion"><h3>More About Meesho </h3></button>
+            <span >
+            <i class="fa-solid fa-angle-down" id="buttonChange"></i>
+            </span>
+            <div></div>
+        
+            <div id="aboutMeeshoContent">
+            <h3>Meesho: Online Shopping Made Affordable</h3>
+            <p>There are lots of reasons to shop online! It can be fun and convenient. And if you are looking for budget-friendly
+            online shopping, then you are at the right place. Meesho offers top-quality products at rock bottom prices, allowing you
+            to shop to your heart’s content without any budget restraints.</p>
+            <h3>Explore a Wide Range of Products Across All Categories</h3>
+            <p>At Meesho, you have over 50 lakh items to choose from including a wide range of apparel, cosmetics, household items, and
+            many more categories! We have 650+ product categories and thousands of handpicked products that are the latest on the
+            market. Whether you're looking for a new outfit, a gift for a special someone, or just some household items for everyday
+            use, you will find everything you need at the lowest prices on Meesho.</p>
+            <h3>The Best Of Women's Fashion at Your Fingertips</h3>
+            <h3>Western Wear for Women</h3>
+            <p>Shopping for Western wear for women at Meesho is always economical as we only bring you the latest styles at wholesale prices. Affordability without compromising on the style quotient is possible at Meesho where you will find a huge range of casual, formal, and partywear dresses, gowns, jeans, tops, T-shirts, skirts, and more. You can buy all the trending outfits you want along with funky jewellery and accessories at reasonable prices</p>
+            <h3>Women’s Ethnic Wear Collection</h3>
+            <p>No matter what occasion or season, you’ll find the perfect outfits in our latest women’s ethnic wear collection. From
+            traditional Sarees and Blouses to classy Suits, and casual Kurtis, we have it all. Picking the perfect outfit for your
+            next wedding party or festive celebration is effortless when you shop with us! Check out our stunning collection of
+            accessories, footwear jewellery sets, and earrings to go with your ethnic look.</p>
+            <h3>Jewellery, Accessories, and Footwear</h3>
+            <p>It's true that accessories make or break your look! When you're getting dressed, it's good to always have some
+            accessories, footwear, and jewellery that compliment your outfit.
+            
+            But you don't need to shell out too much money to get the latest designs anymore.
+            
+            With Meesho, you can buy different types of accessories like bags, watches, stoles, necklaces, earrings, bracelets, and
+            more at affordable prices.</p>
+            <h3>The Latest Collection of Men’s Fashion Just a Click Away</h3>
+            <h3>Western Wear for Men</h3>
+            <p>From formal wear like suits, trousers, men’s shirts, and coats to casual wear like polos and button-down shirts, we have
+            the latest designs in every category. Our selection of menswear is sure to please even the pickiest male customers.
+            These products come in a range of sizes & colors to fit your body & style.</p> 
+            <h3>Men's Ethnic Wear</h3>
+            <p>Do you need a new Sherwani? Are you heading to a wedding and want to look your best? If you answered yes, look no
+            further than our latest men’s ethnic wear collection! Whether you want a Kurta set, Pajamas, Sherwani set, or any other
+            Indian attire, we have an array of budget-friendly styles that are sure to fit your needs.</p>
+            <h3>Accessories, Footwear, and more</h3>
+            <p>In our collection of accessories for men, you will find all sorts of items like watches, keychains, wallets, and more!
+            Our footwear selection is constantly updated with the latest trends in shoes so that you never have to miss out on
+            anything fashionable. If you're looking for some casual and comfortable sneakers, sports shoes, or smart and formal
+            shoes for the workplace, we’re sure to have just the thing that will match your requirements.</p>
+            <h3>Best Quality Beauty Products at Low Prices</h3>
+            <p>We know you don't have time to run around town trying to find the best makeup or the best products for your skincare
+            routine. This is why we sell everything from lipsticks, foundations, compacts to daily use face wash, moisturizers,
+            sunscreens, and everything in between. Browse through our enormous collection of products to find the best skincare and
+            makeup products for your needs.</p>
+            <h3>Trendy Kidswear for Your Little Ones</h3>
+            <p>If you're looking to buy some new outfits or cute accessories for your kids, be sure to check out our kidswear
+            collection. You will find the latest kids’ rompers, skirts, tops,dresses, and frocks for girls to adorable clothing
+            sets, jeans, shorts,topwear, etc for your boys. We also have ethnic wear and accessories for kids of all ages. Our baby
+            care collection includes everything you might need to take care of your little one.</p>
+            <h3>An Exciting Range of Home Essentials, Kitchen Products, Electronics, and more
+            Electronics and Accessories</h3>
+            <p>If you are looking for affordable gadgets and everyday essentials like chargers, headphones, data cables, etc, we have a
+            huge selection you can browse. With great deals and sales happening all the time, your wallet will be happy, too. Check
+            out the deals we've picked especially for you, and you'll find great gifts for everyone on your list. Less is more with
+            our electronic items, and you'll feel the savings, too.</p>
+            <h3>Kitchenware</h3>
+            <p>We carry all of the essentials that you'll need to spice up your kitchen. You'll have a wide variety of utensils and
+            cookware to choose from, so you could pick something that fits your needs and your budget. We also have kitchen
+            accessories like storage jars and containers, small appliances like juicers, mixers, induction cooktops that will make
+            your cooking a whole lot easier! We have a wide range of serveware and cutlery including glasses, dinner sets, knives to
+            choose from, so you'll be able to find what you need at a great price.</p>
+            <h3>Home Essentials</h3>
+            <p>We know how crucial it is to give your home a makeover every once in a while. However, it is important to ensure that
+            you're investing your money in the right products. With Meesho, you'll find the best prices and the best selections,
+            where we have home furnishing items like bedsheets, curtains, blankets, etc in every color you could possibly want and
+            we're always making sure that you can get your hands on the best home decor items, with the biggest discounts. So be
+            sure to check out our home decor section.</p>
+            <h3>Sports, Health & Fitness</h3>
+            <p>It is now more important than ever to keep your body fit and healthy. So start your fitness journey with Meesho. Check
+            out our selection of fitness equipment available at wholesale prices. You will find everything from sportswear to yoga
+            mats to exercise equipment for your home workouts like dumbbell sets, resistance bands, etc.</p>
+            <h3>Stationery Collection</h3>
+            <p>You do not need to spend a lot of money on buying new stationery. We have a large collection of stationery items for
+            various uses and the usage is for everyone, whether you are a student or a working professional. We have a wide variety
+            of beautiful notebooks, diaries, pens, pencils, postcards, sticky notes, and much more.</p>
+            <h3>Use Meesho App for The Best Online Shopping Experience</h3>
+            <p>Now you can shop for anything your heart desires from the comfort of your home. Make sure to download the Meesho app to
+            have a smooth and enjoyable shopping experience. You can even share products you like with your friends on social media
+            in just seconds. If you're trying to save time and get the best deals, downloading the best app for shopping is the way
+            to go!
+            
+            Don't Miss Out! Download Meesho App Now</p>
+            <h3>Become a Reseller: Start Making Money In Three Simple Steps</h3>
+            <p>Now that you've been introduced to Meesho's product categories, it's time to learn about our other offerings. Besides
+            shopping for yourself on the platform, you can also become an entrepreneur and earn cash for what you do best. Here’s
+            how you can begin your entrepreneurial journey.
+            
+            Browse: Use your phone to register a new account, or log in to an existing one at the Meesho app or website. You can
+            read through our catalog and select products that you want at wholesale rates!
+            
+            Share: You can simply send catalogs and product images over Whatsapp, Facebook, or Instagram to your contacts in a
+            single click using the share option.
+            
+            Earn: Once you have shared the catalogs with your prospective customers, you'll soon start getting order requests and
+            inquiries. You can quote product prices with an added margin, which will include a profit for you. Once an order has
+            been placed and paid for, you can place the orders yourself using our established processes that ensure they get
+            delivered to your waiting customers on time.
+            
+            Start Your Online Shopping Journey with Meesho Today
+            
+            You can rest assured that we only deliver the best products to you. You have multiple payment options including debit
+            and credit cards, UPI, and Cash on Delivery. If you're not happy with the quality of your product, send it back and get
+            a full refund without any hassle. Want to learn more? Feel free to send us an email at query@meesho.com. You can also
+            reach out to us on Facebook, Instagram, or Twitter. We're here to help you!</p>
+            <h3>Trending now</h3>
+            <p>Silk Saree | Kajal | Sanitizers | Kurtis Design | Lehenga Designs | Goggles | Suits | Mangalsutra | Gold Earrings |
+            Blazers for Men | Bridal Lehengas | Necklaces | Rakhi | Ring | Salwar Suits | Blouse | Clocks | Girls Jeans | Ladies
+            Watches | Gold Necklace Design | Hair Straighteners | Denim Jackets for Men | Kurta Design | Sexy Maxi Dresses |
+            Noserings | Bed Sheets | Gold Bangles Design | Gold Chain Design | Led Lights | Umbrellas | Payal | Ladies Dress | Gold
+            Mangalsutra Design | Men Jeans | Jewellery Sets | Paithani Sarees | Sofa Covers | Sweaters for Women | Tops Designs |
+            Water Bottles | Wedding Sarees</p>`
+            }
+
+
